e953c29ce8f049abbe6a04c5f34f464f9fd188a6
Foysal Ahmed <mfoysal314@gmail.com>
2024-03-20 02:05:50 +0600

    sign out link added in nav button

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <div className="mx-8 py-5 rounded-md bg-white flex justify-between px-5">
             <h1 className="text-3xl font-bold"><Link to="/">Asset<span className="text-[#6558F5]">Pulse</span></Link></h1>
             {
-                user ? <Link to="/register"><Button className="bg-[#6558F5] hover:bg-[#342d81]">Sign Out</Button></Link>
+                user ? <Link to="/profile/signout"><Button className="bg-[#6558F5] hover:bg-[#342d81]">Sign Out</Button></Link>
                     : <Link to="/register"><Button className="bg-[#6558F5] hover:bg-[#342d81]">Sign Up</Button></Link>
             }
         </div>
